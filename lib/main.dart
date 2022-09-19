6813c2000231e6ad2f4bf1edb3922f6769a14455
dikamuharman <dika1254@gmail.com>
2022-09-19 13:53:44 +0700

    feat: :heavy_plus_sign: add integration firebase to flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_starter_riverpod/firebase_options.dart';
 import 'package:flutter_starter_riverpod/shared/configs/provider.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Hive.initFlutter();
-
   final container = ProviderContainer();
 
   runApp(
